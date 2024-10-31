7cd8509ed187e772706fc649723aeec77fb4533b
jackkkchen <jackkkchen@users.noreply.github.com>
2024-10-31 13:48:55 +0000

    deploy: 更新网站结构和导航 d2154e62860a744d9149911ae8d36327af24b273

---
+++ _nuxt/static/1730382535/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPython-Website\u002F_nuxt\u002Fstatic\u002F1730382535",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FPython-Website\u002F",assetsPath:"\u002FPython-Website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c92cb9ba"}}}}(null));
